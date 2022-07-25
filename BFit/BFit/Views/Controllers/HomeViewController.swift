ba6153939bc0e53d3bf40adf9a641130389475cf
Ahmed Fayek <ahmedashraffayeq@gmail.com>
2022-07-25 20:43:24 +0200

    add pods

--- BFit/BFit/Views/Controllers/HomeViewController.swift
+++ BFit/BFit/Views/Controllers/HomeViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  HomeViewController.swift
 //  BFit
 //
 //  Created by Ahmed Fayeq on 25/07/2022.
@@ -7,13 +7,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class HomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .red
     }
-
-
+    
 }
-
