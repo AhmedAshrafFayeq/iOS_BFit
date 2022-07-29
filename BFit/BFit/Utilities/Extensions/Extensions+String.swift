3ad2f28236499c212648de35e825c776d12a7ff6
Ahmed Fayek <ahmedashraffayeq@gmail.com>
2022-07-29 19:36:25 +0200

    add localization to support English and Deutsch

---
+++ BFit/BFit/Utilities/Extensions/Extensions+String.swift
@@ -0,0 +1,25 @@
+//
+//  Extensions+String.swift
+//  BFit
+//
+//  Created by Ahmed Fayeq on 29/07/2022.
+//
+
+import Foundation
+
+// String extension for Localization
+extension String {
+    func localized(forLanguageCode lanCode: String) -> String {
+        guard
+            let bundlePath = Bundle.main.path(forResource: lanCode, ofType: "lproj"),
+            let bundle = Bundle(path: bundlePath)
+        else { return "" }
+        
+        return NSLocalizedString(
+            self,
+            bundle: bundle,
+            value: " ",
+            comment: ""
+        )
+    }
+}
