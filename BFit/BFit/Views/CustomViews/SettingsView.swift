644c8d579524b174e2c2ad2783ea34875eb3b3dc
Ahmed Fayek <ahmedashraffayeq@gmail.com>
2022-07-30 15:13:46 +0200

    edit resources group

---
+++ BFit/BFit/Views/CustomViews/SettingsView.swift
@@ -0,0 +1,20 @@
+//
+//  SettingsView.swift
+//  BFit
+//
+//  Created by Ahmed Fayeq on 30/07/2022.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SettingsView_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingsView()
+    }
+}
