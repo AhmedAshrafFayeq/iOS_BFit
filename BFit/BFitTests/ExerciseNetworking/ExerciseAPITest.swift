cf41e8472e85b3e15317434fb82cfd9a6f7acc08
Ahmed Fayeq <ahmedashraffayeq@gmail.com>
2024-02-18 17:55:38 +0300

    Add unit testing using Factory DI for ExerciseAPITest

--- BFit/BFitTests/ExerciseNetworking/ExerciseAPITest.swift
+++ BFit/BFitTests/ExerciseNetworking/ExerciseAPITest.swift
@@ -8,6 +8,7 @@
 import XCTest
 import RxSwift
 import RxCocoa
+import Factory
 @testable import BFit
 
 class ExerciseAPITest: XCTestCase {
@@ -16,12 +17,10 @@ class ExerciseAPITest: XCTestCase {
     let disposeBag = DisposeBag()
 
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-        exerciseViewModel = ExerciseViewModel(exerciseAPI: MockExercsieAPI())
+        Container.shared.exerciseAPI.register { MockExercsieAPI() }
     }
 
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
         exerciseViewModel = nil
     }
     
